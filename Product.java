17f597203389c2a1a5bffd4085e10bde7ee2b99e
n-e-g-i-n <109351650+n-e-g-i-n@users.noreply.github.com>
2024-06-23 12:06:19 +0330

    Update Product.java
    I added the Buyer class to the project

--- Product.java
+++ Product.java
@@ -105,3 +105,24 @@ class Main {
         System.out.println("Products sorted by rating: " + sortedByRating);
     }
 }
+class Buyer {
+    private String buyerId;
+    private double balance;
+    private String password;
+
+    public Buyer(String buyerId, double initialBalance, String password) {
+        this.buyerId = buyerId;
+        this.balance = initialBalance;
+        this.password = password;
+    }
+
+
+    public void changePassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void addBalance(double amount) {
+        this.balance += amount;
+    }
+
+}
