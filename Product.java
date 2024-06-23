adb68853bb68b53195c1de1ce98d092773b0ddd3
Ali <100272288+Tali002@users.noreply.github.com>
2024-06-23 13:17:08 +0330

    Update Product.java

--- Product.java
+++ Product.java
@@ -49,9 +49,12 @@ class Product {
 
 class StoreManager {
     private ArrayList<Product> products;
+    private Map<String, String> userCredentials;
 
     public StoreManager() {
         this.products = new ArrayList<>();
+        this.userCredentials = new HashMap<>();
+
     }
 
     public void addProduct(Product product) {
@@ -86,6 +89,13 @@ class StoreManager {
         ArrayList<Product> sortedList = new ArrayList<>(products);
         sortedList.sort((p1, p2) -> Double.compare(p2.getRating(), p1.getRating()));
         return sortedList;
+    }
+    public void registerUser(String username, String hashedPassword) {
+        userCredentials.put(username, hashedPassword);
+    }
+
+    public boolean verifyUserCredentials(String username, String hashedPassword) {
+        return userCredentials.containsKey(username) && userCredentials.get(username).equals(hashedPassword);
     }
 }
 
