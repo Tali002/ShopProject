db50c0c6d4458de47c2556c0dcad701bd44145b3
Ali <100272288+Tali002@users.noreply.github.com>
2024-06-28 08:51:21 +0330

    Update Buyer.java
    updating Buyer class

--- Buyer.java
+++ Buyer.java
@@ -1,12 +1,36 @@
+import java.util.ArrayList;
+
 public class Buyer {
+    private String name;
     private String buyerId;
     private double balance;
     private String password;
+    private ArrayList<Product> cart;
 
-    public Buyer(String buyerId, double initialBalance, String password) {
+
+
+    public Buyer(String name,String buyerId, double initialBalance, String password) {
+        this.name = name;
         this.buyerId = buyerId;
         this.balance = initialBalance;
         this.password = password;
+        this.cart = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBuyerId() {
+        return buyerId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void changePassword(String newPassword) {
@@ -15,5 +39,16 @@ public class Buyer {
 
     public void addBalance(double amount) {
         this.balance += amount;
+    }
+    public void addToCart(Product product) {
+        cart.add(product);
+    }
+
+    public void removeFromCart(Product product) {
+        cart.remove(product);
+    }
+
+    public ArrayList<Product> getCart() {
+        return new ArrayList<>(cart);
     }
 }
